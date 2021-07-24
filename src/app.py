d99a59c65c0032083045cebda93789537e3ab851
sudo-logic <awesomelogix@gmail.com>
2021-07-24 16:40:40 +0530

    added redirect rule for favicon

--- src/app.py
+++ src/app.py
@@ -69,9 +69,9 @@ def wordlist():
 def filter(string):
     return filter_string(string)
 
-@app.route('/favicon.ico')
-def favicon():
-    return app.send_static_file('favicon.ico')
+
+app.add_url_rule('/favicon.ico',
+                 redirect_to=url_for('static', filename='assets/favicon.ico'))
 
 if __name__ == "__main__":
     app.run(debug=True)
