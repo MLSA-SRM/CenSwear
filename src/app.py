0a89393839c3523ded23dffa3c4d2654c8271517
sudo-logic <awesomelogix@gmail.com>
2021-08-09 17:01:47 +0530

    revamped filtration system

--- src/app.py
+++ src/app.py
@@ -8,72 +8,119 @@ from urllib.parse import unquote
 from flask import Flask, json, redirect, render_template, request, url_for, jsonify, abort
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from six import class_types
 
 
 app = Flask(__name__)
 
 limiter = Limiter(app, key_func=get_remote_address)
 
-# os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
 load_dotenv()
 
 WORDLIST_URL = os.environ['WORDLIST_URL']
-filter_words = requests.get(WORDLIST_URL).json()
+CLEAN_WORDLIST_URL = os.environ.get('CLEAN_WORDLIST_URL')
+
+if CLEAN_WORDLIST_URL:
+    clean_wordlist = requests.get(CLEAN_WORDLIST_URL).text.split()
+else:
+    print('Clean Wordlist URL not provided.')
+    clean_wordlist = []
+
+wordlist = requests.get(WORDLIST_URL).json()
 
 censor_symbols = '*'
 
+print('Clean List:',len(clean_wordlist))
+print('Filter List:',len(wordlist))
 
-def filter_string(msg, sec_run=False):
-    if sec_run:
-        space_indexes = [m.start() for m in re.finditer(' ', msg)]
-        msg = msg.replace(' ', '')
-        for word in filter_words:
-            if word.endswith(r'\w*') and sec_run:
-                continue
-            search = re.search(r'\s*{}'.format(word), msg, re.IGNORECASE)
-            if search:
-                print('Run2', msg, search.group(), word)
-                cen = ''.join(
-                    random.choice(censor_symbols) for a in range(len(search.group())))
-                msg = re.sub(r'({})'.format(word),
-                             cen,
-                             msg,
-                             flags=re.IGNORECASE)
-        for i in space_indexes:
-            msg = msg[:i] + ' ' + msg[i:]
+def first_run(s):
+    censor_indices = []
+    for word in wordlist:
+        search = re.search(r'\b({})\b'.format(word), s, re.IGNORECASE)
+        if search:
+            print(search)
+            censor_indices.append((search.start(1), search.end(1)))
+            cen = ''.join(
+                random.choice(censor_symbols) for a in range(len(search.group())))
+            s = re.sub(r'({})'.format(word),
+                       cen,
+                       s,
+                       flags=re.IGNORECASE)
 
-        return msg
-
-    # print(msg)
-    else:
-        censor_indexes = []
-        for word in filter_words:
-            search = re.search(r'\s*({})'.format(word), msg, re.IGNORECASE)
-            if search:
-                print('Run_1 Start:', msg, search.group(), word,
-                      search.start(1), search.end(1))
-                censor_indexes.append((search.start(1), search.end(1)))
-        for i in censor_indexes:
-            msg = msg[:i[0]] + ''.join(random.choice(censor_symbols)
-                                       for c in range(i[1]-i[0])) + msg[i[1]:]
-
-        print('Run_1 End:', msg)
-        return filter_string(msg, sec_run=True)
+    return s
 
 
-@app.route('/')
+def second_run(s):
+    space_indices = [m.start() for m in re.finditer(' ', s)]
+    s = s.replace(' ', '')
+    for word in wordlist:
+        if word.endswith(r'\w*'):
+            continue
+        search = re.search(r'{}'.format(word), s, re.IGNORECASE)
+        if search:
+            print(search)
+            cen = ''.join(
+                random.choice(censor_symbols) for a in range(len(search.group())))
+            s = re.sub(r'({})'.format(word),
+                       cen,
+                       s,
+                       flags=re.IGNORECASE)
+    for i in space_indices:
+        s = s[:i] + ' ' + s[i:]
+
+    return s
+
+
+def third_run(s, base):
+    clean_indices = get_clean_indices(base)
+
+    for i in clean_indices:
+        s = s[:i] + base[i] + s[i+1:]
+    return s
+
+
+def censor(s, indices):
+    for i in indices:
+        s = s[:i[0]] + ''.join(random.choice(censor_symbols)
+                               for c in range(i[1]-i[0])) + s[i[1]:]
+    return s
+
+
+def get_clean_indices(s):
+    clean_indices = []
+    words = s.split()
+    words = list(set(words))
+    for i in words:
+        if len(i) < 3:
+            continue
+        if i.lower() in clean_wordlist:
+            for m in re.finditer(i.lower(), s):
+                clean_indices.extend(list(range(m.start(), m.end())))
+    print(clean_indices)
+    return clean_indices
+
+
+def filter_string(s):
+    base = s
+    s = first_run(s)
+    s = second_run(s)
+    s = third_run(s, base)
+    
+    return s
+
+
+@ app.route('/')
 def index():
     return render_template('home.html')
 
 
-@app.route('/about')
+@ app.route('/about')
 def aboutus():
     return render_template('about.html')
 
 
-@limiter.limit('120 per minute')
-@app.route('/filter', methods=['GET', 'POST'])
+@ limiter.limit('120 per minute')
+@ app.route('/filter', methods=['GET', 'POST'])
 def filter():
     if request.method == 'POST':
         if not request.json or not 'text' in request.json:
